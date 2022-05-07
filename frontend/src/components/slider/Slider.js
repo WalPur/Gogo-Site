c81d6131b77a20de3b0b13067da32c647199aa6d
Okonesh02 <freegod2805@gmail.com>
2022-05-07 13:23:34 +0900

    Updated slider

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -2,7 +2,36 @@ import './slider.css'
 
 function Slider(){
     return(
-        <h1>Hello World!</h1>
+        <div id="sliderExampleIndicators" className="slider slide" data-bs-ride="slider">
+            <div class="slider-indicators">
+                <button type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            </div>
+            <div class="slider-inner">
+                <div className="slider-item active">
+                    <svg src="" className="d-block w-100"/>
+                </div>
+                <div className="slider-item">
+                    <svg src="" className="d-block w-100"/>
+                </div>
+                <div className="slider-item">
+                    <svg src="" className="d-block w-100"/>
+                </div>
+                <div className="slider-item">
+                    <svg src="" className="d-block w-100"/>
+                </div>
+            </div>
+            <button className="slider-control-prev" type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide="prev">
+                <span className="slider-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="slider-control-next" type="button" data-bs-target="#sliderExampleIndicators" data-bs-slide="next">
+                <span className="slider-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
     )
 }
 
