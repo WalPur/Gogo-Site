f3c6240223eb0179a21888cff5316c35fdc884b2
Okonesh02 <freegod2805@gmail.com>
2022-05-07 19:34:48 +0900

    Fixed general

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -1,5 +1,15 @@
+import './About.css'
+
+import iphone from './iphone.svg'
+import macbook from './macbook.svg'
+import samsung from './samsung.svg'
+
 function About(){
-    
+    return(
+        <div>
+            
+        </div>
+    )
 }
 
 export default About;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,13 @@
 import Slider from "../components/slider/Slider";
 import General from "../components/general/General";
+import About from "../components/about/About"
 
 
 function Home() {
     return (
       <div className="Home container">
               <General/>
+              <About />
               <Slider />
       </div>
     );
