0537a9b0393b3a23d409e854a0b448b06fdea51b
Okonesh02 <freegod2805@gmail.com>
2022-05-07 18:53:35 +0900

    Added general component

---
+++ frontend/src/components/general/General.js
@@ -0,0 +1,32 @@
+import './General.css'
+import gogo_icon from './gogo_icon.svg'
+import phone_img from './phone_img.svg'
+import title from './general_title.svg'
+
+function General(){
+    return(
+        <div className='general__block'>
+            <div className='general__content'>
+                <div className='general__title'>
+                    <div className='general__image'>
+                        <img src={title} alt='general title' />
+                    </div>
+                    <p className='general__subtitle'>
+                        Агрегатор курьерской службы
+                    </p>
+                    <p className='general__text'>
+                        Теперь клиенты доступны вам для коммуникаций, за доставку платит клиент, а мы по-прежнему быстро доставляем!
+                    </p>
+                </div>
+            </div>
+            <div className='general__logo'>
+                <img src={gogo_icon} alt='gogo icon' />
+            </div>
+            <div className='general__phone'>
+                <img src={phone_img} alt='phone img' />
+            </div>
+        </div>
+    )
+}
+
+export default General;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,11 @@
 import Slider from "../components/slider/Slider";
+import General from "../components/general/General";
 
 
 function Home() {
     return (
       <div className="Home container">
+              <General/>
               <Slider />
       </div>
     );
