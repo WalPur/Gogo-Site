390e9cd942463009312d67b5aba02793ad6ba5f2
Misha <mishkamba1@gmail.com>
2022-05-08 13:44:56 +0900

    cons refactor

--- frontend/src/components/formPartner/FormPartner.js
+++ frontend/src/components/formPartner/FormPartner.js
@@ -1,50 +1,68 @@
+import React from "react";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
-import "./FormCourier.css"
 
+class FormPartner extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orgName: "",
+            orgCity: "",
+            fullName: "",
+            orgNumber: "",
+            orgEmail: ""
+        };
 
-function FormPartner() {
-    return (
-      <div className="FormPartner">
-          <div className="formTitle">
-            Заявление на подключение заведения
-          </div>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Название организации</Form.Label>
-                <Form.Control type="text" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Город</Form.Label>
-                <Form.Select>
-                    <option disabled>Пожалуйста, выберите</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>ФИО ответственного</Form.Label>
-                <Form.Control type="text" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Номер ответственного</Form.Label>
-                <Form.Control type="text" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Электроннная почта</Form.Label>
-                <Form.Control type="email" placeholder="пример" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Принять
-            </Button>
-            </Form>
-            <div className="agreement">
-            Нажимая на любую кнопку вы соглашаетесь с условиями пользования и политикой конфиденциальности
-            </div>
-      </div>
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+    render () {
+        return (
+            <div className="FormPartner">
+                <div className="formTitle">
+                    Заявление на подключение заведения
+                </div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicOrgName">
+                        <Form.Label>Название организации</Form.Label>
+                        <Form.Control type="text" value={this.state.orgName} onChange={this.handleInputChange} name="orgName" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicOrgCity">
+                        <Form.Label>Город</Form.Label>
+                        <Form.Control type="text" value={this.state.orgCity} onChange={this.handleInputChange} name="orgCity" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicFullName">
+                        <Form.Label>ФИО ответственного</Form.Label>
+                        <Form.Control type="text" value={this.state.fullName} onChange={this.handleInputChange} name="fullName" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicOrgNumber">
+                        <Form.Label>Номер ответственного</Form.Label>
+                        <Form.Control type="text" value={this.state.orgNumber} onChange={this.handleInputChange} name="orgNumber" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Электроннная почта</Form.Label>
+                        <Form.Control type="email" value={this.state.orgEmail} onChange={this.handleInputChange} name="orgEmail" placeholder="пример" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Принять
+                    </Button>
+                    </Form>
+                <div className="agreement">
+                    Нажимая на любую кнопку вы соглашаетесь с условиями пользования и политикой конфиденциальности
+                </div>
+        </div>
     );
+}
   }
   
   export default FormPartner;
