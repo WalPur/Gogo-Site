83f159e126c3cd89e0d3876ea973c5bd6622bbee
Misha <mishkamba1@gmail.com>
2022-05-17 18:52:02 +0900

    form finished

--- backend/frontend/src/components/formCourier/FormCourier.jsx
+++ backend/frontend/src/components/formCourier/FormCourier.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import Input from 'react-phone-number-input/input'
 import axios from "axios";
 import regions from "./data_file.json";
 import { useNavigate } from "react-router-dom";
@@ -16,10 +17,10 @@ function FormCourier() {
     const [number, setNumber] = useState("");
     const [email, setEmail] = useState("");
     const [type, setType] = useState("WALK");
-
     const navigate = useNavigate();
 
-    function submitForm() {
+    function submitForm(e) {
+        e.preventDefault();
         const data = JSON.stringify({
             city: city,
             last_name: last_name,
@@ -42,57 +43,57 @@ function FormCourier() {
             <div className="formTitle">
                 Заявление на курьера
             </div>
-            <Form.Group className="mb-3" controlId="formBasisCourierCity">
-                <Form.Label>Город <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Select required value={city} onChange={e => setCity(e.target.value)} name="courierCity">
-                    <option disabled>Пожалуйста, выберите</option>
-                    {regions.map(region => (
-                        <optgroup label={region.region}>
-                            {region.cities.map(city => (
-                                <option value={city}>
-                                    {city}
-                                </option>
-                            ))}
-                        </optgroup>
-                    ))}
-                </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierSurname">
-                <Form.Label>Фамилия <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Control required type="text" value={last_name} onChange={e => setLastName(e.target.value)} name="courierLastName" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierName">
-                <Form.Label>Имя <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Control required type="text" value={first_name} onChange={e => setFirstName(e.target.value)} name="courierFirstName" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierPatronymic">
-                <Form.Label>Отчество <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Control required type="text" value={middle_name} onChange={e => setMiddleName(e.target.value)} name="courierMiddlename" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierNumber">
-                <Form.Label>Номер телефона <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Control required type="text" value={number} onChange={e => setNumber(e.target.value)} name="courierNumber" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierEmail">
-                <Form.Label>Электроннная почта <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Control required type="email" value={email} onChange={e => setEmail(e.target.value)} name="courierEmail" placeholder="пример" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCourierType">
-                <Form.Label>Тип курьеров <span className="requiredFormStar">*</span></Form.Label>
-                <Form.Select required value={type} onChange={e => setType(e.target.value)} name="courierType">
-                    <option disabled>Пожалуйста, выберите</option>
-                    <option value="WALK">Пеший</option>
-                    <option value="BYCYCLE">Велосипед</option>
-                    <option value="MOTORBIKE">Мотоциклист</option>
-                    <option value="CAR">Автомобилист</option>
-                </Form.Select>
-            </Form.Group>
-            <Button variant="primary" onClick={submitForm}>
-                Принять
-            </Button>
-            <div className="agreement">
-                Нажимая на любую кнопку вы соглашаетесь с условиями пользования и политикой конфиденциальности
-            </div>
+            <Form onSubmit={submitForm}>
+                <Form.Group className="mb-3" controlId="formBasisCourierCity">
+                    <Form.Label>Город <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Select required value={city} onChange={e => setCity(e.target.value)} name="courierCity">
+                        {regions.map(region => (
+                            <optgroup label={region.region}>
+                                {region.cities.map(city => (
+                                    <option value={city} key={city}>
+                                        {city}
+                                    </option>
+                                ))}
+                            </optgroup>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierSurname">
+                    <Form.Label>Фамилия <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Control required type="text" value={last_name} onChange={e => setLastName(e.target.value)} name="courierLastName" placeholder="пример" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierName">
+                    <Form.Label>Имя <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Control required type="text" value={first_name} onChange={e => setFirstName(e.target.value)} name="courierFirstName" placeholder="пример" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierPatronymic">
+                    <Form.Label>Отчество <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Control required type="text" value={middle_name} onChange={e => setMiddleName(e.target.value)} name="courierMiddleName" placeholder="пример" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierNumber">
+                    <Form.Label>Номер телефона <span className="requiredFormStar">*</span></Form.Label>
+                    <Input placeholder="пример" pattern=".{16}" value={number} onChange={setNumber} className="form-control" international country="RU" withCountryCallingCode id="formBasicCourierNumber"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierEmail">
+                    <Form.Label>Электроннная почта <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Control required type="email" value={email} onChange={e => setEmail(e.target.value)} name="courierEmail" placeholder="пример" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCourierType">
+                    <Form.Label>Тип курьеров <span className="requiredFormStar">*</span></Form.Label>
+                    <Form.Select required value={type} onChange={e => setType(e.target.value)} name="courierType">
+                        <option value="WALK">Пеший</option>
+                        <option value="BYCYCLE">Велосипед</option>
+                        <option value="MOTORBIKE">Мотоциклист</option>
+                        <option value="CAR">Автомобилист</option>
+                    </Form.Select>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Принять
+                </Button>
+                <div className="agreement">
+                    Нажимая на любую кнопку вы соглашаетесь с условиями пользования и политикой конфиденциальности
+                </div>
+            </Form>
         </div>
     )
 }
--- backend/frontend/src/components/formPartner/FormPartner.jsx
+++ backend/frontend/src/components/formPartner/FormPartner.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import Input from 'react-phone-number-input/input'
 import axios from "axios";
 import regions from "./data_file.json";
 import { useNavigate } from "react-router-dom";
 
 function FormPartner() {
     const [org_name, setOrg_name] = useState("");
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState("Москва");
     const [full_name, setFull_name] = useState("");
     const [org_number, setOrg_number] = useState("");
     const [org_email, setOrg_email] = useState("");
 
     const navigate = useNavigate();
 
-    function submitForm() {
+    function submitForm(e) {
+        e.preventDefault();
         const data = JSON.stringify({
             org_name: org_name,
             city: city,
@@ -29,7 +31,6 @@ function FormPartner() {
         const url = process.env.REACT_APP_PARTNER_URL;
         axios.post(url, data, { headers })
             .then(response => navigate("/recieved/" + response.data.id))
-        
     }
 
     return (
@@ -37,39 +38,41 @@ function FormPartner() {
                 <div className="formTitle">
                     Заявление на подключение заведения
                 </div>
-                <Form.Group className="mb-3" controlId="formBasicOrgName">
-                    <Form.Label>Название организации</Form.Label>
-                    <Form.Control required type="text" value={org_name} onChange={e => setOrg_name(e.target.value)} name="orgName" placeholder="пример" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicOrgCity">
-                    <Form.Label>Город</Form.Label>
-                    <Form.Select required value={city} onChange={e => setCity(e.target.value)} data-live-search="true" className="selectpicker" name="courierCity">
-                        {regions.map(region => (
-                            <optgroup label={region.region}>
-                                {region.cities.map(city => (
-                                    <option value={city}>
-                                        {city}
-                                    </option>
-                                ))}
-                            </optgroup>
-                        ))}
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicFullName">
-                    <Form.Label>ФИО ответственного</Form.Label>
-                    <Form.Control required type="text" value={full_name} onChange={e => setFull_name(e.target.value)} name="fullName" placeholder="пример" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicOrgNumber">
-                    <Form.Label>Номер ответственного</Form.Label>
-                    <Form.Control required type="text" value={org_number} onChange={e => setOrg_number(e.target.value)} name="orgNumber" placeholder="пример" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Электроннная почта</Form.Label>
-                    <Form.Control required type="email" value={org_email} onChange={e => setOrg_email(e.target.value)} name="orgEmail" placeholder="пример" />
-                </Form.Group>
-                <Button variant="primary" onClick={submitForm}>
-                    Принять
-                </Button>
+                <Form onSubmit={submitForm}>
+                    <Form.Group className="mb-3" controlId="formBasicOrgName">
+                        <Form.Label>Название организации</Form.Label>
+                        <Form.Control required type="text" value={org_name} onChange={e => setOrg_name(e.target.value)} name="orgName" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicOrgCity">
+                        <Form.Label>Город</Form.Label>
+                        <Form.Select required value={city} onChange={e => setCity(e.target.value)} data-live-search="true" className="selectpicker" name="courierCity">
+                            {regions.map(region => (
+                                <optgroup label={region.region}>
+                                    {region.cities.map(city => (
+                                        <option value={city}>
+                                            {city}
+                                        </option>
+                                    ))}
+                                </optgroup>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicFullName">
+                        <Form.Label>ФИО ответственного</Form.Label>
+                        <Form.Control required type="text" value={full_name} onChange={e => setFull_name(e.target.value)} name="fullName" placeholder="пример" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicOrgNumber">
+                        <Form.Label>Номер ответственного</Form.Label>
+                        <Input placeholder="пример" pattern=".{16}" value={org_number} onChange={setOrg_number} className="form-control" international country="RU" withCountryCallingCode id="formBasicOrgNumber"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Электроннная почта</Form.Label>
+                        <Form.Control required type="email" value={org_email} onChange={e => setOrg_email(e.target.value)} name="orgEmail" placeholder="пример" />
+                    </Form.Group>
+                    <Button type="submit" variant="primary">
+                        Принять
+                    </Button>
+                </Form>
                 <div className="agreement">
                     Нажимая на любую кнопку вы соглашаетесь с условиями пользования и политикой конфиденциальности
                 </div>
