7ce2c169d6d1beaa96520abfc91ac48ca7f3c2b6
Misha <mishkamba1@gmail.com>
2022-05-23 16:28:09 +0900

    header update

--- backend/frontend/src/components/header/Header.jsx
+++ backend/frontend/src/components/header/Header.jsx
@@ -1,30 +1,30 @@
 import "./Header.css";
 
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
+
 import { NavLink } from 'react-router-dom';
 import logo from "./logo.png";
 
 function Header() {
     return (
         <header className="container-fluid">
-            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/">
+            <NavLink className={({ isActive }) => (isActive ? 'active nav-link logo-img' : 'inactive nav-link logo-img')}  to="/">
                 <img src={logo}/>
             </NavLink>
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
-                    <span className="navbar-brand"><b>Gogo</b>Курьер</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/">ГЛАВНАЯ</NavLink>
-                        <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/docs">ДОКУМЕНТЫ</NavLink>
-                        <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/courier">ЗАЯВКА ДЛЯ КУРЬЕРОВ</NavLink>
-                        <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/partner">ЗАЯВКА ДЛЯ ПАРТНЕРОВ</NavLink>
-                    </div>
-                    </div>
-                </div>
-            </nav>
+            <Navbar expand="lg" className="navbar navbar-expand-lg navbar-light">
+                <Container>
+                    <Navbar.Brand href="/"><span className="navbar-brand"><b>Gogo</b>Курьер</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto navbar-nav">
+                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/">ГЛАВНАЯ</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/docs">ДОКУМЕНТЫ</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/courier">ЗАЯВКА ДЛЯ КУРЬЕРОВ</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/partner">ЗАЯВКА ДЛЯ ПАРТНЕРОВ</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     );
 }
