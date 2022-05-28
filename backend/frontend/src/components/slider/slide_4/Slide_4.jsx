9a84ac92228db06e24cb1d1aea0f9219a748ae0f
Misha <mishkamba1@gmail.com>
2022-05-28 19:07:47 +0900

    fourth slide is now adaptive

--- backend/frontend/src/components/slider/slide_4/Slide_4.jsx
+++ backend/frontend/src/components/slider/slide_4/Slide_4.jsx
@@ -1,6 +1,10 @@
 import "./Slide_4.css";
 
-import group from "./group.png"
+import courier_1 from "./courier_1.png";
+import courier_2 from "./courier_2.png";
+import courier_3 from "./courier_3.png";
+import courier_4 from "./courier_4.png";
+import group from "./group.png";
 
 function Slide_4() {
     return (
@@ -9,6 +13,16 @@ function Slide_4() {
                 <span className="Slide_4_bold">Курьеры</span><span className="GogoSpan"> Gogo</span>Курьер
             </div>
             <img src={group} className="courier_group"/>
+            <div className="couriers">
+                <div className="couriers_1">
+                    <img src={courier_1} className="courier" />
+                    <img src={courier_2} className="courier" />
+                </div>
+                <div className="couriers_2">
+                    <img src={courier_3} className="courier" />
+                    <img src={courier_4} className="courier" />
+                </div>
+            </div>
         </div>
     )
 }
